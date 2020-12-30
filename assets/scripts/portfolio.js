b21faec6a3a2defffadec016b387174fd37de3fc
Marcel Ray <mr@marcelray.com>
2020-12-29 19:21:12 -0700

    Add initial revision

---
+++ assets/scripts/portfolio.js
@@ -0,0 +1,94 @@
+document.addEventListener("DOMContentLoaded", () => app.start());
+window.addEventListener('hashchange', () => app.onHashChange(), false);
+
+class Project {
+	id = ""
+	category = ""
+	colophone = ""
+	date = "2000-01-01"
+	description = ""
+	display = false
+	media = []
+	links = []
+	responsibilities = ""
+	tags = []
+	title = "test"
+	color = "#ff00ff"
+	imageUrl = "https://source.unsplash.com/random"
+	client = "marcelray"
+
+	static getFromJson(json) {
+		let project = new Project();
+		for (let prop in json) {
+			project[prop] = json[prop];
+		}
+		return project;
+	}
+}
+
+const app = new Vue({
+	el: '#mr--app',
+	data: {
+		projects: [],
+		projectGroups: [],
+		currentProject: null,
+	},
+	methods: {
+		start: function() {
+			fetch('assets/data/projects.json').then(response => response.json()).then((json) => this.onProjectDataLoaded(json));
+		},
+		showProjectDetails: function(project) {
+			this.currentProject = project;
+			$('body,html').addClass('mr--no-scroll');
+		},
+		hideProjectDetails: function() {
+			location.hash = 'home';
+			this.currentProject = null;
+			$('body,html').removeClass('mr--no-scroll');
+		},
+		getGroupProjects: function(group) {
+			return this.projects.filter(item=>{
+				return group.categories.indexOf( item.category ) >= 0;
+			});
+		},
+		onProjectDataLoaded: function(json) {
+			// console.log(json);
+			json.projects.forEach((item, index) => {
+				this.projects.push(Project.getFromJson(item));
+			})
+			json.groupings.forEach((item, index) => {
+				this.projectGroups.push(item);
+			})
+		
+			if ( location.hash.length > 0 ) {
+				this.onHashChange();
+			}
+		},
+		// Handle hash changes (browser back, deep link, etc.)
+		onHashChange: function() {
+			const projectId = location.hash.substr(1);
+			// TODO: Optimize this
+			const matchingProjects = this.projects.filter( item => {
+				return item.id === projectId;
+			});
+			if ( matchingProjects.length === 1 ) {
+				this.showProjectDetails( matchingProjects[0] );
+			} else {
+				this.hideProjectDetails();
+			}
+		}
+	},
+	computed: {
+		currentProjectVideos: function() {
+			return this.currentProject.media.filter(item => {
+				return item.type === "video";
+			});
+		},
+		currentProjectImages: function() {
+			return this.currentProject.media.filter(item => {
+				return item.type === "image";
+			});
+		}
+	}
+})
+
---
+++ resume.php
@@ -0,0 +1,324 @@
+<?php
+include_once( "assets/partials/_definitions.php" );
+
+$pageID = $PAGE_RESUME;
+
+include_once( "assets/partials/_header.php" );
+?>
+
+<style>
+		@media print {
+
+			.job-detail.employer , .job-detail.employer a {
+				color: #09c !important;
+			}
+		}
+</style>
+
+
+<article id="page-resume">
+
+	<h2>R&eacute;sum&eacute;</h2>
+
+<!-- CONTACT -->
+
+	<div id="resume">
+
+		<header>
+
+			<div class="header-group">
+				<h2>Marcel Ray</h2>
+				<h3>Design Technologist</h3>
+			</div>
+			<div id="contactInfo">
+				<p><a href="mailto:mr@marcelray.com">mr@marcelray.com</a></p>
+				<p class="dates"><a href="http://marcelray.com/" target="_blank">www.marcelray.com</a></p>
+			</div>
+
+		</header>
+
+	<!-- SUMMARY -->
+
+		<section id="resume-section-summary">
+
+			<h2>Summary</h2>
+				<p>Design-minded technologist focused on prototyping and building user interfaces, spatial experiences, and design tools with an emphasis on those that connect physical and digital worlds.</p>
+		</section>
+
+	<!-- SKILLS -->
+
+		<section id="resume-section-skills">
+
+			<h2>Skills + Tools</h2>
+
+			<p>
+			Web technologies (JavaScript/Vue.js/Node.js/React/Cordova), C#/.NET, Unity 3D, augmented and virtual reality interactions (HoloLens/Oculus VR/ARKit), bespoke hardware integration, inter-device communication, UX design, design/motion integration, IoT integration, electronics prototyping.
+			</p>
+
+		</section>
+
+	<!-- EMPLOYMENT -->
+
+		<section id="resume-section-employment">
+
+			<h2>Experience</h2>
+
+			<ol>
+
+			<li class="item">
+					<h3 class="job-title">Visual Information Specialist / Park Ranger</h3>
+					<div class="job-details">
+						<div class="job-detail employer"><a href="http://nps.gov/noca/" rel="shadowbox" target="_blank">North Cascades National Park</a></div>
+						<div class="job-detail location">Marblemount, WA</div>
+						<div class="job-detail dates">Jun 2019 to Present</div>
+					</div>
+					<div class="job-description">
+						<p>Developed a public-facing real-time backcountry camp availability web tool, improved UX on web-based permitting system, designed an in-person COVID-safe permitting station with integrated digital display and interactive map; designed physical park signage; performed backcountry field patrols.</p>
+					</div>
+				</li>
+
+			<!--
+				<li class="item">
+					<h3 class="job-title">Technical Consultant</h3>
+					<div class="job-details">
+						<div class="job-detail employer">(Various)</div>
+						<div class="job-detail dates">Nov 2015 to Present</div>
+					</div>
+					<div class="job-description">
+						<p>Provide interactive development, prototyping and/or technical guidance for various startups and large organizations including the National Park Service, the National Audubon Society and Techstars Startup Weekend.</p>
+					</div>
+				</li>
+			-->
+
+				<li class="item">
+					<h3 class="job-title">Design Technologist (AR/VR)</h3>
+					<div class="job-details">
+						<div class="job-detail employer"><a href="https://www.oculus.com/blog/inventing-the-future/" rel="shadowbox" target="_blank">Oculus Research</a></div>
+						<div class="job-detail location">Redmond, WA</div>
+						<div class="job-detail dates">Jan 2018 to May 2019</div>
+					</div>
+					<div class="job-description">
+						<p>Defined, prototyped, and tested new digital and physical interactions for the future of augmented and virtual realities using bespoke hardware prototypes and off-the-shelf devices. These interactive prototypes and demos were critical for senior executive leadership to evaluate the potential of radically new input/output paradigms.</p>
+					</div>
+				</li>
+
+				<li class="item">
+					<h3 class="job-title">Senior Design Developer (Contract)</h3>
+					<div class="job-details">
+						<div class="job-detail employer"><a href="http://aka.ms/HackingSTEM/" rel="shadowbox" target="_blank">Microsoft Hacking STEM Education</a></div>
+						<div class="job-detail location">Redmond, WA</div>
+						<div class="job-detail dates">Mar 2017 to Oct 2017</div>
+					</div>
+
+					<div class="job-description">
+						<ul>
+							<li>Driving force behind a major C# code migration and UI redesign of the Microsoft Excel Data Streamer add-in used by STEM educators and students around the world to visualize real-time data from Arduino microcontrollers.</p>
+						</ul>
+					</div>
+				</li>
+
+				<li class="item">
+					<h3 class="job-title">Senior Design Technologist</h3>
+					<div class="job-details">
+						<div class="job-detail employer"><a href="http://amazon.com/b?node=16008589011" target="_blank">Amazon Go</a></div>
+						<div class="job-detail location">Seattle, WA</div>
+						<div class="job-detail dates">Aug 2014 to Oct 2015</div>
+					</div>
+
+					<div class="job-description">
+						<p>Spearheaded the design technology program and spatial prototyping processes for Amazon&apos;s innovative physical retail concept centered around a checkout-free shopping experience. 
+							<!--Established rapid prototyping and &ldquo;Wizard of Oz&rdquo; environment simulation as invaluable mediums for design exploration and high fidelity testing of experiences that weren&apos;t yet technically possible. Collaborated regularly with VPs, researchers, engineers and other senior level members in influencing project direction.-->
+						</p>
+						<!--<p><span class="star">&starf;</span> Awarded an &ldquo;Outstanding&rdquo; performance review for my impacts, bestowed upon only the top 5% Design Technologists at Amazon.-->
+					</div>
+				</li>
+
+				<div class="pageBreak"></div>
+
+				<li class="item">
+					<h3 class="job-title">Senior Design Prototyper (Contract)</h3>
+					<div class="job-details">
+						<div class="job-detail employer"><a href="https://www.microsoft.com/en-us/band" target="_blank">Microsoft Connected Car</a>, <a href="https://www.microsoft.com/en-us/band" target="_blank">Microsoft Band</a></div>
+						<div class="job-detail location">Redmond, WA</div>
+						<div class="job-detail dates">Sept 2012 to Jul 2014</div>
+					</div>
+
+					<div class="job-description">
+						<p>Collaborated with researchers and designers on the design and implementation of visual and UI flow explorations for cutting-edge consumer products. Rapidly developed high fidelity, end-to-end prototypes which were critical for formal usability testing as well as presentations to senior-leadership and corporate partners.</p>
+					</div>
+				</li>
+
+				<li class="item">
+					<h3 class="job-title">Technical Lead &amp; Interactive Developer</h3>
+					<div class="job-details">
+						<div class="job-detail employer"><a href="http://www.bwco.info/" rel="shadowbox" target="_blank">Belle &amp; Wissell</a></div>
+						<div class="job-detail location">Seattle, WA</div>
+						<div class="job-detail dates">Jun 2010 to Aug 2012</div>
+					</div>
+
+					<div class="job-description">
+						<p>Oversaw technical direction and managed development resources for building rich interactive exhibits and installations for nationally and internationally recognized museums and environments. <!--Interfaced with clients and their technical staffs, architected a kiosk development kit and researched emerging technologies.--></p>
+					</div>
+				</li>
+
+				<li class="item">
+					<h3 class="job-title">API Designer (Contract)</h3>
+					<div class="job-details">
+						<div class="job-detail employer"><a href="https://www.nintendo.com/" target="_blank">Nintendo of America</a></div>
+						<div class="job-detail location">Redmond, WA</div>
+						<div class="job-detail dates">Mar 2009 to Dec 2009</div>
+					</div>
+
+					<div class="job-description">
+						<p>Designed the official Flash-Lite API for content providers and partners—including the BBC–to build custom apps with video playback for the Nintendo Wii.</p>
+					</div>
+				</li>
+
+				<li class="item">
+					<h3 class="job-title">Interactive Developer</h3>
+					<div class="job-details">
+						<div class="job-detail employer"><a href="http://www.smashingideas.com/" target="_blank">Smashing Ideas, Inc.</a></div>
+						<div class="job-detail location">Seattle, WA</div>
+						<div class="job-detail dates">Mar 2006 to Oct 2007</div>
+					</div>
+					<div class="job-description print-exclude">
+						<p>Developed richly interactive Flash websites and games for clients including Disney Channel, Nintendo, Nickelodeon and Starbucks.</p>
+					</div>
+				</li>
+
+				<li class="item">
+					<h3 class="job-title">Camera Interface Developer (Contract)</h3>
+					<div class="job-details">
+						<div class="job-detail employer"><a href="http://kodak.com" target="_blank">Eastman Kodak</a></div>
+						<div class="job-detail location">Rochester, NY</div>
+						<div class="job-detail dates">Dec 2004 to Feb 2006</div>
+					</div>
+
+					<div class="job-description print-exclude">
+						<p>Front-end interface developer on the Kodak EasyShare-One, the first consumer digital camera with touchscreen and Wi-Fi.</p>
+					</div>
+				</li>
+
+				<li class="item">
+					<h3 class="job-title">Interactive Developer &amp; Designer</h3>
+					<div class="job-details">
+						<div class="job-detail employer">Buck &amp; Pulleyn</div>
+						<div class="job-detail location">Pittsford, NY</div>
+						<div class="job-detail dates">Aug 2004 to Dec 2004</div>
+					</div>
+					<div class="job-description print-exclude">
+						<p>
+							Developed and designed interactive campaigns within a regionally and nationally
+							acclaimed advertising agency for clients including
+							Kodak, Ithaca College and Corning Glass.
+						</p>
+					</div>
+				</li>
+
+				<li class="item">
+					<h3 class="job-title">Interactive Developer</h3>
+					<div class="job-details">
+						<div class="job-detail employer">Element K</div>
+						<div class="job-detail location">Rochester, NY</div>
+						<div class="job-detail dates">Oct 2003 to Jul 2004</div>
+					</div>
+					<div class="job-description print-exclude">
+						<p>
+							Developed custom Flash components for generating instructional graphics and animations for enterprise e-learning courses.
+						</p>
+					</div>
+				</li>
+			</ol>
+
+		</section>
+
+	<!-- VOLUNTEERING -->
+
+		<section id="resume-section-volunteering" style="display: none;">
+
+			<h2>Volunteering</h2>
+
+			<ol>
+				<li class="item">
+					<h3 class="job-title">Wilderness Ranger Intern + Volunteer</h3>
+					<div class="job-details">
+						<div class="job-detail employer"><a href="www.nps.gov/noca/" target="_blank">North Cascades National Park</a></div>
+						<div class="job-detail location">Marblemount, WA</div>
+						<div class="job-detail dates">Jun&ndash;Sept 2016 / Jun&ndash;Sept 2017</div>
+					</div>
+					<!--
+					<div class="job-description">
+						I spent one full summer as a Wilderness Ranger at North Cascades National Park, patrolling many of the park&apos;s backcountry trails and routes and teaching Leave No Trace backcountry ethics to the public formally and informally. I also spent Septembers of 2016 and 2017 staffing a remote cabin along the Pacific Crest Trail, as part of a special project to better understand the experience and trends of PCT thru hikers.
+					</div>
+				-->
+				</li>
+				<li class="item">
+					<h3 class="job-title">Volunteer Wilderness Ranger &amp; Mountain Steward</h3>
+					<div class="job-details">
+						<div class="job-detail employer">United States Forest Service</div>
+						<div class="job-detail location">Skykomish, WA / Glacier, WA</div>
+						<div class="job-detail dates">Jun&ndash;Sept 2014 / Jun&ndash;Sept 2015</div>
+					</div>
+					<div class="job-description">
+						
+					</div>
+				</li>
+				<li class="item">
+					<h3 class="job-title">Volunteer Trail Crew</h3>
+					<div class="job-details">
+						<div class="job-detail employer">Washington Trails Association</div>
+						<div class="job-detail location">(Various), WA</div>
+						<div class="job-detail dates">Jun 2015 to Sept 2015</div>
+					</div>
+					<div class="job-description">
+						
+					</div>
+				</li>
+			</ol>
+
+		</section>
+
+	<!-- EDUCATION -->
+
+		<section id="resume-section-education">
+
+			<h2>Education</h2>
+			<ol>
+				<li class="item">
+					<h3 class="job-title">B.S. in New Media: Interactive Development</h3>
+					<div class="job-details">
+						<div class="job-detail employer"><a href="http://www.rit.edu/" target="_blank">Rochester Institute of Technology</a></div>
+						<div class="job-detail location">Rochester, NY</div>
+						<div class="job-detail dates">May 2003</div>
+					</div>
+				</li>
+			</ol>
+		</section>
+
+		<!-- HOBBIES-->
+
+<!--
+		<section id="resume-section-interests">
+
+			<h2>Hobbies</h2>
+			<p>Backpacking, nature, Maker projects (Arduino, Raspberry Pi) and learning automotive mechanics and electronics on my ever ailing 1987 Volkswagen Westfalia.</p>
+
+		</section>
+-->
+
+	</div>
+
+	<footer id="resume-footer" class="footer-buttonBar">
+
+		<div class="buttonBar">
+            <a href="assets/downloads/MarcelRay_Resume.pdf" target="_blank" class="button file-pdf">PDF</a>
+            <a href="assets/downloads/MarcelRay_Resume.txt" target="_blank" class="button file-text">Text</a>
+        </div>
+
+    </footer>
+
+</article>
+
+<?php
+include_once( "assets/partials/_footer.php" );
+?>
